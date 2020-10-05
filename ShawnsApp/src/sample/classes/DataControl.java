5354e8238ee06a9811c1348cc016f9087e1fdb6a
Kaloyan <epicvergz@gmail.com>
2020-10-05 21:58:20 +0200

    literally nothing

---
+++ ShawnsApp/src/sample/classes/DataControl.java
@@ -0,0 +1,12 @@
+package sample.classes;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DataControl {
+
+    
+
+}
