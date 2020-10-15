a694e6f9639d9e37ae6b73c4f6fff8c9ba92ff7a
Georgiev,Aleksandar A.N <aleksandar.georgiev@student.fontys.nl>
2020-10-15 20:00:32 +0200

    I thing I did it I thing I migrated it to gradle

---
+++ ShawnAppGradle/ShawnAppGradle/src/main/java/ShawnAppGradle/ShawnAppGradleApplication.java
@@ -0,0 +1,13 @@
+package ShawnAppGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShawnAppGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShawnAppGradleApplication.class, args);
+	}
+
+}
