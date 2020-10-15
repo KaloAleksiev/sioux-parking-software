69ff98c4f0243c26cf9730fc8644527a951c050e
Georgiev,Aleksandar A.N <aleksandar.georgiev@student.fontys.nl>
2020-10-15 20:46:09 +0200

    sample added

---
+++ ShawnAppGradle/ShawnAppGradle/src/sample/classes/Driver.java
@@ -0,0 +1,32 @@
+package sample.classes;
+
+public class Driver {
+    int id;
+    String licencePlate;
+    String phoneNumber;
+    String name;
+
+    public int getId() { return this.id; }
+    public String getLicencePlate() { return licencePlate; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public String getName() { return name; }
+
+    public Driver(String licencePlate, String phoneNumber, String name) {
+        this.licencePlate = licencePlate;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+
+    public Driver(String licencePlate, String phoneNumber, String name, int id) {
+        this.id = id;
+        this.licencePlate = licencePlate;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+
+    public String GetInfo() {
+        return "Name: " + this.name +
+                ", licence plate number: " + this.licencePlate +
+                ", phone number: " + this.phoneNumber;
+    }
+}
---
+++ ShawnAppGradle/ShawnAppGradle/src/sample/classes/DriverController.java
@@ -0,0 +1,61 @@
+package sample.classes;
+
+import sample.classes.Driver;
+
+import javax.xml.crypto.Data;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DriverController {
+    List<Driver> driverList;
+    DataControl dc;
+
+    public DriverController() {
+        dc = new DataControl();
+        driverList = new ArrayList<>();
+    }
+
+    public void addDriver(Driver driver) {
+        driverList.add(driver);
+    }
+
+    public List<Driver> getAllDrivers() {
+        return driverList;
+    }
+
+    public Driver getLastAddedDriver() {
+        return driverList.get(driverList.size() - 1);
+    }
+
+    public boolean removeDriver(int id) {
+        for (Driver driver : driverList) {
+            if (driver.getId() == id) {
+                driverList.remove(driver);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void AddDriverToDB(String phone, String plate, String name) {
+        dc.AddDriverToDB(plate, phone, name);
+    }
+
+    public int GetMaxDriverID() throws SQLException {
+        return dc.GetMaxDriverID();
+    }
+
+    public List<Driver> GetDriversFromDB() throws SQLException {
+        return dc.GetDrivers();
+    }
+
+    public Driver GetDriverById(int id) {
+        for (Driver driver : driverList ) {
+            if (driver.getId() == id) {
+                return driver;
+            }
+        }
+        return null;
+    }
+}
